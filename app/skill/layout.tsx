7d2f00f0458e216abf9582e3bc21cd1b6b8fb8ce
learningenglishgeuwat <learningenglishgeuwat@gmail.com>
2026-03-25 15:37:14 +0700

    Show game links button on skill page

--- app/skill/layout.tsx
+++ app/skill/layout.tsx
@@ -1,6 +1,11 @@
 import RequireActive from '../components/RequireActive'
+import SkillGameButton from './components/SkillGameButton'
 
 export default function SkillLayout({ children }: { children: React.ReactNode }) {
-  return <RequireActive>{children}</RequireActive>
+  return (
+    <RequireActive>
+      {children}
+      <SkillGameButton />
+    </RequireActive>
+  )
 }
-
